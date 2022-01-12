6ef7b124f770b2a2254d297dd7cd909e32a8c071
ifzal22 <enjoyyourlife2244@gmailm>
2022-01-12 10:51:11 +0600

    PUSH

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-
-const { MongoClient, OrderedBulkOperation } = require('mongodb');
+const { MongoClient  } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+
+const cors = require("cors");
 require('dotenv').config();
+
+const app = express();
+const port =  process.env.PORT || 5000;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqdph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -13,12 +21,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-const app = express();
-const port =  process.env.PORT || 5000;
-
-
-app.use(cors());
-app.use(express.json());
 
 
 app.get("/",(req, res)=>{
